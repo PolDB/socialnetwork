c19c891a382005d4e4514810952b2e1e692dd899
Paul <u@example.com>
2024-05-27 13:45:37 +0200

    boucle foreach intégrée

--- controllers/controller.php
+++ controllers/controller.php
@@ -34,11 +34,12 @@ switch ($action) {
 
     // ===================HARDCODED PART===========================
     // format idPost => array of comments
-    $comments[1] = array(
-      array("nickname" => "FakeUser1", "created_at" => "1970-01-01 00:00:00", "content" => "Fake comment 01."),
-      array("nickname" => "FakeUser2", "created_at" => "1970-01-02 00:00:00", "content" => "Fake comment 02."),
-      array("nickname" => "FakeUser1", "created_at" => "1970-01-03 00:00:00", "content" => "Fake comment 03.")
-    );
+    $comments[1] = array();
+    foreach ($posts as $onePost) {
+      $id_post = $onePost['id'];
+      $commentsForThisPost = GetAllPosts($id_post);
+    }
+
     $comments[3] = array(
       array("nickname" => "FakeUser1", "created_at" => "1970-01-01 00:00:00", "content" => "Fake comment 04."),
     );
