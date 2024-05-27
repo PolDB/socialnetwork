cc174c2a486344754d8e3d3ccf69cecbc927ca7a
Paul <u@example.com>
2024-05-27 14:53:07 +0200

    fin exercice 3

--- models/UserManager.php
+++ models/UserManager.php
@@ -14,3 +14,19 @@ function GetAllUsers()
   $response = $PDO->query("SELECT * FROM user ORDER BY nickname ASC");
   return $response->fetchAll();
 }
+
+function GetUserIdFromUserAndPassword($username, $password)
+{
+  global $PDO;
+
+  $sqlQuery = 'SELECT * FROM user WHERE nickname = :nickname';
+  $pdo_prepare = $PDO->prepare($sqlQuery);
+  $pdo_prepare->execute(['nickname' => $username]);
+  $user = $pdo_prepare->fetch();
+
+  if ($user && $_POST['password'] === $user['password']) {
+    return $_SESSION['id'] = ['userId'];
+  } else {
+    return -1;
+  }
+}
